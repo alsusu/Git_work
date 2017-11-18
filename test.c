15c3f2f7e28a3554d9c4bfc7e80a14962bdf3e78
susu <2350724453@qq.com>
2017-11-18 11:25:11 +0800

    this is a test file!

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: alsusu
+	> Mail: 2350724453@qq.com 
+	> Created Time: 2017年11月18日 星期六 11时17分27秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main (char argc, char **argv)
+{
+	print("hello world!\r\n");
+	return 0;
+}
